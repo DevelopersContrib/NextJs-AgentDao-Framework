595ce9c240bbbefdfa43edd5cf5d22bb0084ca87
Andre Bon Cipriano <andreboncipriano@gmail.com>
2024-09-09 08:53:17 +0800

    Updates

--- components/Ai.jsx
+++ components/Ai.jsx
@@ -5,16 +5,16 @@ import Script from 'next/script';
 function Ai() {
   return (
     <>
-      <div className="ai-script pb-custom" style={{ width: '800px', margin: '0 auto' }}></div>
+      <div className="ai-script pb-custom" style={{ width: '100%', margin: '0 auto' }}></div>
       <Script src="https://tools.contrib.com/cwidget/ai?container=ai-script" />
 
       <style>{`
         .ai-script {
-          width: 800px;
+          width: 100%;
           margin: 0 auto;
         }
         .pb-\[88px\] {
-          padding-bottom: 30px;
+          padding-bottom: 30px !important;
         }
         #m0ojcl6w48wku {
           display: none;
